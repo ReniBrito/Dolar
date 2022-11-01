b7d22519bd2e36a0227d30e37cb6232f7193346f
Reni Brito <114774905+ReniBrito@users.noreply.github.com>
2022-11-01 10:02:37 -0300

    Add files via upload

---
+++ ex10 dolar $.py
@@ -0,0 +1,5 @@
+real = float(input('Quanto dinheiro vc tem na carteira ? '))
+dolar = real / 5.168
+print('Com esse valor daria para comprar {:.2f} dolar'.format(dolar))
+print('=='*20)
+print('Contando que o valor do dolar esta a $5.168')
